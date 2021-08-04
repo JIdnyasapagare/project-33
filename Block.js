b5f26c388b8bbade4dbcde1787754609094566d4
JIdnyasapagare <84508221+JIdnyasapagare@users.noreply.github.com>
2021-08-04 17:33:04 +0530

    Update Block.js

--- Block.js
+++ Block.js
@@ -18,8 +18,7 @@ class Block {
         rectMode(CENTER);
         translate(pos.x,pos.y);
         rotate(angle);
-        fill(color);
-        stroke(border);
+        
         strokeWeight(4);
         rect(0,0,this.width,this.height);
         pop();
