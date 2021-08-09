b54a77d088ee734bda4deec6698e729e568c076f
JIdnyasapagare <84508221+JIdnyasapagare@users.noreply.github.com>
2021-08-09 09:16:48 +0530

    Update Block.js

--- Block.js
+++ Block.js
@@ -1,33 +1,26 @@
 class Block {
-    constructor(x,y,width,height){
-        var options={
-            isStatic : false
-        }
-        this.visibility = 255;
-        this.width = width;
-        this.height = height;
-        this.body = Bodies.rectangle(x,y,width,height,options);
-
-        World.add(world,this.body)
-    }
-    display(color,border){
-       var pos = this.body.position;
-       var angle = this.angle; 
-       push();
-    if (this.body.speed < 3){
-        rectMode(CENTER);
-        translate(pos.x,pos.y);
-        rotate(angle);
-        
-        strokeWeight(4);
-        rect(0,0,this.width,this.height);
-        pop();
-    }else {
-        push();
-        World.remove(world,this.body);
-        this.visibility = this.visibility - 1;
-        tint(255,this.visibility);
-        pop();
-    }
-}
+constructor(x,y,width,height){ 
+var options={
+isStatic : false 
+} 
+this.width = width; 
+this.height = height;
+this.body = Bodies.rectangle(x,y,this.width,this.height,options);
+World.add(world,this.body) 
+} 
+display(){
+var pos = this.body.position;
+var angle = this.body.angle;
+if (this.body.speed < 5){
+push();
+translate(pos.x,pos.y);
+rotate(angle);
+rectMode(CENTER);
+strokeWeight(4);
+rect(0,0,this.width,this.height);
+pop();
+}else {
+World.remove(world,this.body); 
+} 
+} 
 }
