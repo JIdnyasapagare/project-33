0ca0b35af3bd8d9b6224a276d9886927be49cfeb
JIdnyasapagare <84508221+JIdnyasapagare@users.noreply.github.com>
2021-08-09 09:11:41 +0530

    Update Polygon.js

--- Polygon.js
+++ Polygon.js
@@ -1,27 +1,25 @@
-class Polygon {
-    constructor(x,y,width,radius){
-        var options = {
-            isStatic : false,
-            restitution:0.5,
-            density:0.2,
-        }
-        this.body = Bodies.circle(x,y,radius,options);
-        this.radius = radius;
-        this.image = loadImage("polygon.png")
-
-        World.add(world,this.body)
-    }
-    display(color,border){
-        var pos = this.body.position;
-        var angle = this.angle;
-        push();
-        ellipseMode(RADIUS);
-        translate(pos.x,pos.y);
-        
-        strokeWeight(4);
-        ellipse(0,0,this.radius,this.radius);
-        pop();
-        imageMode(CENTER)
-        image(this.image,this.body.position.x,this.body.position.y,this.width,this.height)
-    }
+class Polygon { 
+constructor(x,y,radius) { 
+var options = {
+isStatic : false,
+restitution:0.5,
+density:0.2,
+} 
+this.body = Bodies.circle(x,y,radius,options); 
+this.radius = radius;
+this.image = loadImage("polygon.png")
+World.add(world,this.body)
+} 
+display(){
+var pos = this.body.position;
+var angle = this.body.angle;
+push();
+translate(pos.x,pos.y);
+rotate(angle)
+strokeWeight(4); 
+imageMode(CENTER) 
+image(this.image,0,0,this.radius,this.radius)
+pop(); 
+} 
 }
+
